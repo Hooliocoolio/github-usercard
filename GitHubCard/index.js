69ff381fc265bd5f32826f8bbeb4a1bbbdb12c0c
HoolioCoolio <hoolio.osuna@gmail.com>
2020-04-16 10:35:10 -0700

    completed adding my own card

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,36 +2,29 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-const entryPoint = document.querySelector('.cards')
+
+let entryPoint = document.querySelector('.cards')
 
 axios.get("http://api.github.com/users/hooliocoolio")
-  .then( (response) => {
-    response.message.forEach((gitUser) => {
-      const newUserCard = gitHubUserCard(gitUser, data)
-      entryPoint.appendChild(newUserCard);
-    })
+  .then(function (results) {
+    const data = gitHubUserCard(results.data)
+    entryPoint.append(data);
   })
-  // handle success
   .catch(function (error) {
-    // handle error
+    console.log('failed');
     console.log(error);
-  })
-  .then(function () {
-    // always executed
   });
 
-// .then((response) => {
-//   // 3. Once we receive that data, we want to create some components. 
-//   response.data.message.forEach((data) => {
-//     const newUserCard = gitHubUserCard(data);
-//     // 4. Add the components to the screen. 
-//     entryPoint.appendChild(newUserCard);
-//   })
-// })
-// .catch((err) => {
-//   console.log("the data was not returned", err)
-// });
+// .then(function (results) {
+//   // handle success
+//   const data = gitHubUserCard(results.data);
 
+//   entryPoint.append(data);
+// })
+// .catch(function (error) {
+//   // handle error
+//   console.log(error);
+// })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -39,10 +32,7 @@ axios.get("http://api.github.com/users/hooliocoolio")
    Skip to Step 3.
 */
 
-// const axios = require('axios').default;
-// const axios = require('axios');
 
-// Make a request for a user with a given ID
 
 
 
@@ -85,7 +75,7 @@ const followersArray = [];
 */
 
 
-function gitHubUserCard(user, data) {
+function gitHubUserCard(data) {
   // HTML Elements and the classes
   const userCard = document.createElement("div");
   const avatarImg = document.createElement("img");
@@ -99,7 +89,7 @@ function gitHubUserCard(user, data) {
   const following = document.createElement("p");
   const bio = document.createElement("p");
 
-
+  // Set Classes
   userCard.classList.add('card');
   avatarImg.classList.add('userImg');
   info.classList.add('card-info');
@@ -113,8 +103,8 @@ function gitHubUserCard(user, data) {
   bio.classList.add('bio');
 
   // HTML Elements Structure
-  card.appendChild(avatarImg);
-  card.appendChild(cardInfo);
+  userCard.appendChild(avatarImg);
+  userCard.appendChild(info);
   info.appendChild(realName);
   info.appendChild(userName);
   info.appendChild(located);
@@ -130,10 +120,16 @@ function gitHubUserCard(user, data) {
   avatarImg.src = data.avatar_url;
   realName.textContent = data.name;
   userName.textContent = data.login;
-  located.textContent = data.location;
-  profile.textContent = data.url;
+  located.textContent = `Location: ${data.location}`;
+  gitHubUrl.setAttribute("href", data.html_url);
+  gitHubUrl.textContent = data.html_url;
+  profile.textContent = "Profile: ";
+  profile.appendChild(gitHubUrl);
+  followers.textContent = `Followers: ${data.followers}`;
+  following.textContent = `Following: ${data.following}`;
+  bio.textContent = `Bio: ${data.bio}`;
 
-
+  return userCard;
 
 }
 
